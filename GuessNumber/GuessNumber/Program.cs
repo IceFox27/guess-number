7c0464b6add506633596a030efd042b33b67dc2c
IceFox27 <132928954+IceFox27@users.noreply.github.com>
2025-02-25 18:46:44 +1000

    Add random number calculating

---
+++ GuessNumber/GuessNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace GuessNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в игру \"Угадай Число\"!");
+
+            string randomNumber = GetRandomNumberString();
+
+            Console.WriteLine("Я сгенерировал 4-х значное число, цифры в нём не повторяются. " +
+                "Попробуй угадать его. " +
+                "Вводи число только с неповтояющимися цифрами!");
+        }
+
+        static string GetRandomNumberString()
+        {
+            Random random = new Random();
+
+            int firstDigit = random.Next(0, 10);
+            List<int> digits = new List<int> { firstDigit };
+
+            while (digits.Count != 4)
+            {
+                int digit = random.Next(0, 10);
+
+                if (!digits.Contains(digit))
+                {
+                    digits.Add(digit);
+                }
+            }
+
+            string numberString = string.Join("", digits);
+            return numberString;
+        }
+    }
+}

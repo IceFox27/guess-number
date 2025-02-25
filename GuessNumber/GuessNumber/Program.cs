27dbe85fb13b0ae032e71823685f273b446a58ad
IceFox27 <132928954+IceFox27@users.noreply.github.com>
2025-02-25 19:23:03 +1000

    Add game logic

--- GuessNumber/GuessNumber/Program.cs
+++ GuessNumber/GuessNumber/Program.cs
@@ -6,11 +6,29 @@
         {
             Console.WriteLine("Добро пожаловать в игру \"Угадай Число\"!");
 
-            string randomNumber = GetRandomNumberString();
+            GameLogic();
 
-            Console.WriteLine("Я сгенерировал 4-х значное число, цифры в нём не повторяются. " +
-                "Попробуй угадать его. " +
-                "Вводи число только с неповтояющимися цифрами!");
+            while (true)
+            {
+                Console.WriteLine("Продолжим?");
+                Console.WriteLine("1) Да");
+                Console.WriteLine("2) Нет");
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        GameLogic();
+                        break;
+                    case "2":
+                        Console.WriteLine("Спасибо за игру! До свидания!");
+                        return;
+                    default:
+                        Console.WriteLine("Неправильный ввод. Пожалуйста, выберите 1 или 2.");
+                        break;
+                }
+            }
         }
 
         static string GetRandomNumberString()
@@ -32,6 +50,52 @@
 
             string numberString = string.Join("", digits);
             return numberString;
+        }
+
+        static void GameLogic()
+        {
+            string randomNumber = GetRandomNumberString();
+
+            Console.WriteLine("Я сгенерировал 4-х значное число, цифры в нём не повторяются. " +
+                "Попробуй угадать его. " +
+                "Вводи число только с неповтояющимися цифрами!");
+
+            while (true)
+            {
+                Console.WriteLine("Введи своё предположение: ");
+
+                string guess = Console.ReadLine();
+
+                if (guess.Length != 4)
+                {
+                    Console.WriteLine("Неправильный формат. Введите 4 цифры.");
+                    continue;
+                }
+
+                int rightPos = 0;
+                int incorrectPos = 0;
+
+                for (int i = 0; i < 4; i++)
+                {
+                    if (guess[i] == randomNumber.ToString()[i])
+                    {
+                        rightPos++;
+                    }
+                    else if (randomNumber.ToString().Contains(guess[i]))
+                    {
+                        incorrectPos++;
+                    }
+                }
+
+                Console.WriteLine($"Угадано на правильных позициях: {rightPos}, " +
+                            $"угадано на неправильных позициях: {incorrectPos}");
+
+                if (rightPos == 4)
+                {
+                    Console.WriteLine("Поздравляю, вы угадали число!");
+                    break;
+                }
+            }
         }
     }
 }

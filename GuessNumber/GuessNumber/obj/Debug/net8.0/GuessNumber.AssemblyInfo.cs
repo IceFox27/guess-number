f65f2df9c6dbb4379c84a3e73b390ddc76583809
IceFox27 <132928954+IceFox27@users.noreply.github.com>
2025-02-26 10:21:20 +1000

    Add read me photo

--- GuessNumber/GuessNumber/obj/Debug/net8.0/GuessNumber.AssemblyInfo.cs
+++ GuessNumber/GuessNumber/obj/Debug/net8.0/GuessNumber.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuessNumber")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b19ad06183f5bfd8f96aa4cf0647b5f4c917e70f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16a6e3c640142e3b64d8988569b631f5c394c3f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuessNumber")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuessNumber")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
